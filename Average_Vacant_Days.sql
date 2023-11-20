132dafdf8b1b0bb99530fb2325af1a10c0340ec5
Faitus Jeline Joseph <65098339+faitusjelinej@users.noreply.github.com>
2023-11-20 13:58:21 -0800

    Create Average_Vacant_Days.sql

---
+++ Average_Vacant_Days.sql
@@ -0,0 +1,11 @@
+with vacancies as (select l.listing_id, 365 - COALESCE(sum(CASE WHEN checkout_date> '12/31/2021' THEN '12/31/2021' ELSE checkout_date END -
+CASE WHEN checkin_date< '01/01/2021' THEN '01/01/2021' ELSE checkin_date END ), 0) as vacant_days
+from listings l
+left join bookings b
+on l.listing_id = b.listing_id
+where l.is_active = 1
+GROUP BY l.listing_id )
+
+select ROUND(AVG(vacant_days))
+from vacancies
+;
