b25afae4a34320cb754801d3e09fedeb25bcd077
Faitus Jeline Joseph <65098339+faitusjelinej@users.noreply.github.com>
2023-11-20 13:58:56 -0800

    Update Average_Vacant_Days.sql

--- Average_Vacant_Days.sql
+++ Average_Vacant_Days.sql
@@ -1,3 +1,5 @@
+-- https://datalemur.com/questions/average-vacant-days
+
 with vacancies as (select l.listing_id, 365 - COALESCE(sum(CASE WHEN checkout_date> '12/31/2021' THEN '12/31/2021' ELSE checkout_date END -
 CASE WHEN checkin_date< '01/01/2021' THEN '01/01/2021' ELSE checkin_date END ), 0) as vacant_days
 from listings l
